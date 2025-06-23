1e8dbc8ef8cee91b689227d4a06ce85460c34142
Nour Sadek <nour.sadek@mail.utoronto.ca>
2025-06-23 11:47:18 +0300

    Added a script that would filter the protein sequences so that only the longest isoform of each protein would be kept

--- download_files.py
+++ download_files.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import ftplib
+import gzip
+import os
+from Bio import SeqIO
+
 
 # Define FTP server, urls, and WBPS release number
 FTP_HOST = "ftp.ebi.ac.uk"
@@ -35,7 +39,6 @@ ftp = ftplib.FTP(FTP_HOST)
 ftp.login()
 
 # Download the annotations file (gff3) and the genomic and protein sequences for each clade V species
-i = 1
 for species, bioproject_id in clade_V_genomes_info.items():
     # Change the name of the species to a FTP server compatible one
     species_name = species.lower().replace("_", "").split(" ")
@@ -50,9 +53,56 @@ for species, bioproject_id in clade_V_genomes_info.items():
         for file_type in FILE_TYPES:
             matching_files = [f for f in filenames if f.endswith(file_type)]
             for filename in matching_files:
-                print(f"Downloading {i}: {filename} from {dir_path}")
+                print(f"Downloading {filename} from {dir_path}")
                 with open(filename, "wb") as f:
                     ftp.retrbinary(f"RETR {filename}", f.write)
     except ftplib.error_perm:
         print(f"Directory not found: {dir_path}")
-    i = i + 1
+
+
+# Filter the protein files so that only the longest protein isoform is kept
+
+PROTEINS_DIR = "./protein_sequences"
+FILTERED_PROTEINS_DIR = "./filtered_protein_sequences"
+
+
+def get_gene_id(description):
+    description_list = description.split()  # split by any kind of whitespace
+    # first check for the presence of the "gene" descriptor
+    for item in description_list:
+        if item.startswith("gene="):
+            return item[5:]
+    # if the "gene" descriptor does not exist, then search for the "transcript" descriptor
+    for item in description_list:
+        if item.startswith("transcript="):
+            return item[11:]
+    # if neither exist, return None
+    return None
+
+
+def keep_longest_isoform(file):
+    basename = file.replace(".protein.fa.gz", "")
+    input_path = os.path.join(PROTEINS_DIR, file)
+    output_path = f"{FILTERED_PROTEINS_DIR}/{basename}_filtered.protein.fa"
+
+    longest_isoforms = {}
+
+    with gzip.open(input_path, "rt") as handle:
+        for record in SeqIO.parse(handle, "fasta"):
+            gene_id = get_gene_id(record.description)
+
+            current_longest = longest_isoforms.get(gene_id)
+
+            if not current_longest or len(record.seq) > len(current_longest.seq):
+                longest_isoforms[gene_id] = record
+
+    with open(output_path, "w") as out_handle:
+        SeqIO.write(longest_isoforms.values(), out_handle, "fasta")
+
+
+i = 1
+for file in os.listdir(PROTEINS_DIR):
+    if file.endswith(".protein.fa.gz"):
+        keep_longest_isoform(file)
+        print(f"Saved filtered file {i} for {file}")
+        i = i + 1
